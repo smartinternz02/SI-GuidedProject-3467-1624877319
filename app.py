1f49e3409fc622990b4e8a48fd1b774d49d7607d
Manikanth Tarine <83544043+manikanththarine@users.noreply.github.com>
2021-07-26 21:07:17 +0530

    app

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
+from monkeylearn import MonkeyLearn
 import os 
 
 app = Flask(__name__)
-x = pickle.load(open('F:\keywords ext\keywords extraction.ipynb', 'rb'))
+x = pickle.load(open('keywords extraction.ipynb','r'))
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+	return render_template("index.html")
 
 @app.route('/extraction',methods=['POST'])
 def extraction():
     p = request.form['a ']
     print(p)
-    return render_template('extraction.html')
+    return render_template('keywordextractionapii.py')
 
 if __name__ == "__main__":
     app.run(debug=True)
